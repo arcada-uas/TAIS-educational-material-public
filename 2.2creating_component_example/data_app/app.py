644ec862e8200ffe083d5b4249ee8c2dbf65c351
LottaHann <lotta.hannelius@aalto.fi>
2024-07-08 17:09:13 +0300

    updated requirements and app to fix import bugs

--- 2.2creating_component_example/data_app/app.py
+++ 2.2creating_component_example/data_app/app.py
@@ -10,7 +10,7 @@ current_dir = os.path.dirname(os.path.abspath(__file__))
 parent_dir = os.path.join(current_dir, '../')
 sys.path.append(parent_dir)
 
-from data_app.data_service import clean_data
+from data_service import clean_data
 
 CLEANED_DATA = {}
 
